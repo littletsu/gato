e505456304ab89deaab5b74c9b3983fed7586155
Littletsu <38116582+littletsu@users.noreply.github.com>
2023-03-13 14:03:15 -0600

    Test bg

--- entity/Background.lua
+++ entity/Background.lua
@@ -2,7 +2,7 @@ local AssetManager = require("engine.AssetManager")
 local paths = require("engine.AssetPaths")
 
 local Background = {
-    current_background = AssetManager:loadImage(paths.backgrounds .. "bg0.png"),
+    current_background = AssetManager:loadImage(paths.backgrounds .. "bg1.png"),
     
     draw = function(self) 
         love.graphics.draw(self.current_background)
